1d99cae537df6055be89829213e43889d03b4b08
juan <juan@PAT-000180>
2026-03-03 15:15:45 -0300

    projeto rodando

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
@@ -14,9 +14,12 @@ const geistMono = Geist_Mono({
 export default function Home() {
   return (
     <div
-      className={`${geistSans.className} ${geistMono.className} flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black`}
+      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-
+      [20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20
+       gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
-      <h1>Hello World</h1>
+      <h2>Hello World</h2>
+      <Button>Button</Button>
     </div>
   );
 }
