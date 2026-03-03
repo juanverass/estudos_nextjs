7819b002b37f1fa78df9acd397f8f5e91150c5c0
juan <juan@PAT-000180>
2026-03-03 16:56:13 -0300

    Header interativo

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,8 @@
 import Link from "next/link"
-import { Button } from "../ui/button"
-import { useRouter } from "next/router"
-import { cn } from "@/lib/utils";
+import { ActiveLink } from "../active-link";
+import { Button } from "../ui/button";
 
 export const Header = () => {
-const router = useRouter();
-const isHomePage = router.pathname === '/';
-const isBlogPage = router.pathname.startsWith('/blog');
-
     return (
         <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95
         backdrop-blur supports-backdrop-filters:bg-background/60">
@@ -16,9 +11,13 @@ const isBlogPage = router.pathname.startsWith('/blog');
                     <Link href="/">Logo</Link>
 
                     <nav className="flex items-center gap-6">
-                        <Link href="/"  className={cn('text-sm font-medium transition-colors hover:text-blue-500', isHomePage ? 'text-blue-500' : 'text-muted-foreground')}>Inicio</Link>
-                        <Link href="/blog" className={cn('text-sm font-medium transition-colors hover:text-blue-500', isBlogPage ? 'text-blue-500' : 'text-muted-foreground')}>Blog</Link>
-                        <Button variant="secondary">Começar</Button>
+                        <ActiveLink href="/">Início</ActiveLink>
+                        <ActiveLink href="/blog">Blog</ActiveLink>
+                        <Button variant="secondary" asChild>
+                            <Link href="/comecar">
+                                Começar
+                            </Link>
+                        </Button>
                     </nav>
                 </div>
             </div>
